e053b9723850d49aeae04cd6707c2a23026fba86
Karmicatosaurus <dev@karmicat.fr>
2025-08-02 18:51:34 +0200

    Réecriture du script

--- config_site_lamp.sh
+++ config_site_lamp.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-# --- Aide ---
 if [ -z "$1" ]; then
-    echo "Utilisation : $0 nom_du_projet [--writable] [--ssl] [--mvc] [--tout]"
+    echo "Utilisation : $0 nom_du_projet [--writable] [--mvc] [--symfony]"
+    echo " --writable : Le dossier de publique peut être entre lecture / écriture"
+    echo " --mvc : Créer une structure MVC"
+    echo " --symfony : Mets en place le framework symfony"
+    echo "Les paramètres --mvc et --symfony ne peuvent être utilisés en même temps."
     printf "\033[31mCe script doit être exécuté en tant que root !\033[0m\n"
     exit 1
 fi
@@ -16,56 +19,51 @@ fi
 
 # --- Paramètres ---
 PROJET="$1"
-USER_NAME="${SUDO_USER}"
-DOMAINE="${PROJET}.local"
-DOSSIER="/srv/web/${PROJET}"
-CONF_HTTP="/etc/httpd/conf.d/${PROJET}.conf"
-CONF_SSL="/etc/httpd/conf.d/${PROJET}-ssl.conf"
+UTILISATEUR="$SUDO_USER"
+DOMAINE="$PROJET.local"
+DOSSIER_RACINE="/srv/web"
+DOSSIER_PUB="$DOSSIER_RACINE/$PROJET"
+CONF_HTTP="/etc/httpd/conf.d/$PROJET.conf"
+CONF_SSL="/etc/httpd/conf.d/$PROJET-ssl.conf"
 SSL_DIR="/etc/httpd/ssl"
-CRT="${SSL_DIR}/${DOMAINE}.crt"
-KEY="${SSL_DIR}/${DOMAINE}.key"
+CRT="$SSL_DIR/$DOMAINE.crt"
+KEY="$SSL_DIR/$DOMAINE.key"
 
 WRITABLE=false
-ENABLE_SSL=false
 MVC=false
+SYMFONY=false
 
 # --- Analyse des options ---
 for arg in "$@"; do
   case "$arg" in
     --writable) WRITABLE=true ;;
-    --ssl) ENABLE_SSL=true ;;
-    --mvc) MVC=true;;
-    --tout)
-        WRITABLE=true
-        ENABLE_SSL=true
-        MVC=true
-        ;;
+    --mvc) MVC=true ;;
+    --symfony) SYMFONY=true ;;
   esac
 done
 
+if [[ "$MVC" == true && "$SYMFONY" == true ]]; then
+  printf "\033[31mErreur : Impossible d'utiliser --mvc et --symfony en même temps !\033[0m\n"
+  exit 1
+fi
+
 echo "Création du projet web: $PROJET"
 
-# --- Création du dossier ---
-echo "Création du dossier $DOSSIER"
-mkdir -p "$DOSSIER"
+if [[ ! -d "$DOSSIER_RACINE" ]]; then
+  echo "Création du dossier $DOSSIER_RACINE"
+  mkdir -p "$DOSSIER_RACINE"
+  chown -Rf "$UTILISATEUR":apache $DOSSIER_RACINE
+fi
+
+if [[ -d "$DOSSIER_PUB" ]]; then
+  printf "\033[31mLe dossier %s existe déjà !\033[0m\n" "$DOSSIER_PUB"
+  exit 1
+fi
 
 if $MVC; then
-  # Dossier public
-  mkdir -p "$DOSSIER/public/"
-  mkdir -p "$DOSSIER/public/artsys/"
-  mkdir -p "$DOSSIER/public/artsys/css/"
-  mkdir -p "$DOSSIER/public/artsys/imgs/"
-  mkdir -p "$DOSSIER/public/artsys/js/"
+  mkdir -p "$DOSSIER_PUB"/{public/artsys/{css,imgs,js},src/{Configurations,Controllers,Models,Repository,Views}}
 
-  # Src
-  mkdir -p "$DOSSIER/src/"
-  mkdir -p "$DOSSIER/src/Configurations/"
-  mkdir -p "$DOSSIER/src/Controllers/"
-  mkdir -p "$DOSSIER/src/Models/"
-  mkdir -p "$DOSSIER/src/Repository/"
-  mkdir -p "$DOSSIER/src/Views/"
-
-  tee "$DOSSIER/.htaccess" > /dev/null <<EOF
+  tee "$DOSSIER_PUB/.htaccess" > /dev/null <<EOF
   <IfModule mod_rewrite.c>
       RewriteEngine On
       RewriteBase /
@@ -75,121 +73,120 @@ if $MVC; then
 EOF
 fi
 
-chown -R "$USER_NAME:apache" "$DOSSIER"
-chmod -R 775 "$DOSSIER"
-chmod g+s "$DOSSIER"
+if $SYMFONY; then
+    if [[ ! -f "/usr/local/bin/composer" ]]; then
+      echo "Téléchargement de composer ..."  
+      php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+      
+      HASH_ORIGINE=$(curl -s https://composer.github.io/installer.sig)
+      HASH_FICHIER=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
-# --- SELinux ---
+      if [ "$HASH_ORIGINE" != "$HASH_FICHIER" ]; then
+          echo "Le fichier téléchargé n'est pas correct !";
+          rm composer-setup.php
+          exit 1
+      fi
+
+      php composer-setup.php --install_dir=/usr/local/bin --filename=composer
+      rm composer-setup.php        
+    fi
+
+    sudo -u "$UTILISATEUR" composer create-project --no-interaction --working-dir="$DOSSIER_RACINE" symfony/skeleton "$PROJET"
+    sudo -u "$UTILISATEUR" composer --no-interaction --working-dir="$DOSSIER_PUB" require webapp
+fi
+
+chown -R "$UTILISATEUR":apache "$DOSSIER_PUB"
+chmod g+s "$DOSSIER_PUB"
+
+# --- Config SELinux ---
 if $WRITABLE; then
   echo "SELinux: autorisation en lecture/écriture"
-  chcon -Rt httpd_sys_rw_content_t "$DOSSIER"
+  chcon -Rt httpd_sys_rw_content_t "$DOSSIER_PUB"
 else
   echo "SELinux: autorisation en lecture seule"
-  chcon -Rt httpd_sys_content_t "$DOSSIER"
+  chcon -Rt httpd_sys_content_t "$DOSSIER_PUB"
 fi
 
-# --- Fichier index.php si vide ---
-if [ -z "$(ls -A "$DOSSIER")" ]; then
-  echo "Création d'un index.php de test"
+# --- Activation SSL ---
 
-  if $MVC; then
-  cat <<'EOF' | tee "${DOSSIER}/public/index.php" > /dev/null
-<?php
-echo 'OK';
-EOF
-  chown "$USER_NAME:apache" "${DOSSIER}/public/index.php"
-  chmod 664 "${DOSSIER}/public/index.php"
-  else
-  cat <<'EOF' | tee "${DOSSIER}/index.php" > /dev/null
-<?php
-echo 'OK';
-EOF
-  chown "$USER_NAME:apache" "${DOSSIER}/index.php"
-  chmod 664 "${DOSSIER}/index.php"
-  fi
+echo "Activation SSL pour $DOMAINE"
+
+if [[ ! -d $SSL_DIR ]]; then
+  mkdir -p $SSL_DIR
 fi
 
-# --- Création config HTTP ---
-echo "Création de la config Apache HTTP: $CONF_HTTP"
-if $ENABLE_SSL; then
-  tee "$CONF_HTTP" > /dev/null <<EOF
+echo "Génération du certificat auto-signé"
+openssl req -x509 -nodes -days 365 \
+  -newkey rsa:4096 \
+  -keyout "$KEY" \
+  -out "$CRT" \
+  -subj "/CN=$DOMAINE"
+
+echo "Ajout du certificat dans le store de confiance"
+trust anchor "$CRT"
+
+echo "Création de la config Apache SSL: $CONF_SSL"
+
+tee "$CONF_HTTP" > /dev/null <<EOF
 <VirtualHost *:80>
     ServerName $DOMAINE
     Redirect permanent / https://$DOMAINE/
 </VirtualHost>
 EOF
-else
-  tee "$CONF_HTTP" > /dev/null <<EOF
-<VirtualHost *:80>
-    ServerName $DOMAINE
-    DocumentRoot $DOSSIER
 
-    <Directory $DOSSIER>
-        AllowOverride All
-        Require all granted
-    </Directory>
-
-    ErrorLog logs/${PROJET}-error.log
-    CustomLog logs/${PROJET}-access.log combined
-</VirtualHost>
-EOF
+if $SYMFONY; then
+  DOSSIER_PUB="$DOSSIER_PUB/public"
 fi
 
-# --- SSL ---
-if $ENABLE_SSL; then
-  echo "Activation SSL pour $DOMAINE"
-
-  mkdir -p "$SSL_DIR"
-
-  echo "Génération du certificat auto-signé"
-  openssl req -x509 -nodes -days 365 \
-    -newkey rsa:4096 \
-    -keyout "$KEY" \
-    -out "$CRT" \
-    -subj "/CN=${DOMAINE}"
-
-  echo "Ajout du certificat dans le store de confiance"
-  trust anchor "$CRT"
-
-  echo "Création de la config Apache SSL: $CONF_SSL"
-  tee "$CONF_SSL" > /dev/null <<EOF
+tee "$CONF_SSL" > /dev/null <<EOF
 <VirtualHost *:443>
     ServerName $DOMAINE
-    DocumentRoot $DOSSIER
+    DocumentRoot $DOSSIER_PUB
 
     SSLEngine on
     SSLCertificateFile $CRT
     SSLCertificateKeyFile $KEY
 
-    <Directory $DOSSIER>
+    <Directory $DOSSIER_PUB>
         AllowOverride All
         Require all granted
     </Directory>
 
-    ErrorLog logs/${PROJET}-ssl-error.log
-    CustomLog logs/${PROJET}-ssl-access.log combined
+    ErrorLog logs/$PROJET-ssl-error.log
+    CustomLog logs/$PROJET-ssl-access.log combined
 </VirtualHost>
+EOF
+
+if $SYMFONY; then
+  tee "$DOSSIER_PUB"/.htaccess > /dev/null <<EOF
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+
+    # Ignore les fichiers et dossiers existants
+    RewriteCond %{REQUEST_FILENAME} !-f
+    RewriteCond %{REQUEST_FILENAME} !-d
+
+    # Redirige tout vers index.php
+    RewriteRule ^(.*)$ index.php [QSA,L]
+</IfModule>
 EOF
 fi
 
-# --- /etc/hosts ---
+# --- Ajoût du domaine dans /etc/hosts ---
 if ! grep -q "$DOMAINE" /etc/hosts; then
   echo "Ajout de $DOMAINE dans /etc/hosts"
   echo "127.0.0.1 $DOMAINE" | tee -a /etc/hosts > /dev/null
 fi
 
-# --- Redémarrer Apache ---
 echo "Redémarrage du serveur Apache"
 systemctl restart httpd
 
-# --- Résumé ---
 echo -e "\n\033[32mProjet \"$PROJET\" créé avec succès !\033[0m"
-echo "Dossier : $DOSSIER"
+echo "Dossier : $DOSSIER_PUB"
 echo "Accès HTTP  : http://$DOMAINE"
-if $ENABLE_SSL; then
-  echo "Accès HTTPS : https://$DOMAINE"
-  echo "Redirection HTTP → HTTPS activée"
-fi
+echo "Accès HTTPS : https://$DOMAINE"
+echo "Redirection HTTP → HTTPS activée"
+
 if $WRITABLE; then
   echo "Apache peut écrire dans le dossier."
 fi
